2807818dd4db22375423bb17e54b3b1231d65190
Dane Stuckel <dane.stuckel@gmail.com>
2024-01-12 16:08:40 +0700

    Remove console.log and extra comments.

--- api-keys/src/index.ts
+++ api-keys/src/index.ts
@@ -27,22 +27,8 @@
  */
 
 export interface Env {
-	// ALLOWED_ORIGINS?: string;
-
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+	// See https://developers.cloudflare.com/workers/runtime-apis/kv/
 	API_KEYS: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
 }
 
 const KEY_MIN_SIZE = 10;
@@ -157,7 +143,6 @@ export default {
 			// since browsers may not send the Origin header for GET requests.
 			const allowedOrigins = getAllowedOrigins(env);
 			const origin = request.headers.get('Origin');
-			console.log("JEY", origin, allowedOrigins, "JEY");
 			if (!origin || !allowedOrigins.includes(origin)) {
 				return new Response('Invalid origin', { status: 400 });
 			}
